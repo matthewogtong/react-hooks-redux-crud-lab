d91d2f7d31b79262a36ccdd402f8fc0257b446da
matthewogtong <ogtongm@gmail.com>
2021-03-01 13:26:23 -0500

    commit tree 00aec3db9ec464badd6960bbccd44f052aecf674

--- src/features/restaurants/RestaurantInput.js
+++ src/features/restaurants/RestaurantInput.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux"
+import { restaurantAdded } from "./restaurantsSlice"
 
 function RestaurantInput() {
-  return <div>Restaurant Input</div>;
+
+  const [text, setText] = useState("")
+
+  const dispatch = useDispatch()
+
+  const handleChange = (event) => {
+    setText(event.target.value)
+  }
+
+  console.log(text)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch(restaurantAdded(text))
+    setText("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" value={text} onChange={handleChange}/>
+      <input type="submit" value="Add Restaurant"/>
+    </form>
+  )
 }
 
 export default RestaurantInput;
