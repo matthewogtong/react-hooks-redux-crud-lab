4ecbb0cfca561ebf89ec704fb77125ab460c5b93
matthewogtong <ogtongm@gmail.com>
2021-03-01 13:26:27 -0500

    Done.

--- src/features/reviews/ReviewInput.js
+++ src/features/reviews/ReviewInput.js
@@ -1,8 +1,31 @@
-import React from "react";
-import Reviews from "./Reviews";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux"
+import { reviewAdded } from "./reviewsSlice"
 
-function ReviewInput() {
-  return <div>Review Input</div>;
+function ReviewInput({ restaurantId }) {
+
+  const dispatch = useDispatch()
+
+  const [comment, setComment] = useState("")
+
+  const handleChange = (event) => {
+    setComment(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch(reviewAdded({ comment, restaurantId }))
+    setComment("")
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="comment">comment</label>
+      <input type="text" name="comment" value={comment} onChange={handleChange}/>
+      <input type="submit" value="add review"/>
+    </form>
+  )
 }
 
 export default ReviewInput;
